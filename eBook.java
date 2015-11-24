bc2bbd9e93538f7d1c976e5640bd544c3ec359cc
Kristina Bradbury <bradbukj@users.noreply.github.com>
2015-11-24 02:58:38 -0500

    Update eBook.java

--- eBook.java
+++ eBook.java
@@ -1,3 +1,10 @@
+/*
+* Names:     	   Kristina Bradbury	Emilio Hajj	Jim Wu
+* MacIDs: 	   bradbukj	 	hajje		wuzz
+* Student Numbers: 1405897		1402245		Please see submission comment
+* Description: A class that calls on a public method for retrieving the price of an ebook.
+*/
+
 package Shopping;
 
 public class eBook extends Readable { //The 'eBook' class inherits all the fields and methods of the 'Readadle' class
