f1d0c8c144d7751c282eb0294c5a499209b1e807
emilio-h <hajje@mcmaster.ca>
2015-11-24 02:48:26 -0500

    Update Book.java

--- Book.java
+++ Book.java
@@ -1,3 +1,9 @@
+/*
+* Names:     	   Kristina Bradbury	Emilio Hajj	Jim Wu
+* MacIDs: 	   bradbukj	 	hajje		wuzz
+* Student Numbers: 1405897		1402245		???????
+* Description: A class that calls a public method for retrieving the price of a book 
+*/
 package Shopping;
 
 public class Book extends Readable{ // 'Book' inherits all the fields and methods of the 'Readable' class
