3320276d9b4b5f81441b5a354d0649feda700d6e
Kristina Bradbury <bradbukj@users.noreply.github.com>
2015-11-24 03:27:10 -0500

    Update HWK4_bradbukj.java

--- HWK4_bradbukj.java
+++ HWK4_bradbukj.java
@@ -1,3 +1,10 @@
+/*
+* Names:     	   Kristina Bradbury	Emilio Hajj	Jim Wu
+* MacIDs: 	   bradbukj	 	hajje		wuzz
+* Student Numbers: 1405897		1402245		Please see submission comment
+* Description: A class that calls on a public method to initialize all classes, class variables, and start the first user menu.
+*/
+
 package Shopping;
 public class HWK4_wuzz {
 	public static void main(String[] args) 
