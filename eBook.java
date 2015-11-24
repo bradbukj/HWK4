18bc660c507c02ea0cd9d0e0ef564e1a45817a58
emilio-h <hajje@mcmaster.ca>
2015-11-24 02:26:20 -0500

    Update eBook.java

--- eBook.java
+++ eBook.java
@@ -1,7 +1,7 @@
 package Shopping;
 
 public class eBook extends Readable { //The 'eBook' class inherits all the fields and methods of the 'Readadle' class
-	@Override //Used to override the getPrice() method
+	@Override 
 	public int getPrice() //method to get price
 	{
 		return price;
