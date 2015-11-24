5846d5a98b0520c3004e204bf373bc1b42bab3f2
emilio-h <hajje@mcmaster.ca>
2015-11-24 03:02:09 -0500

    Update MP3.java

--- MP3.java
+++ MP3.java
@@ -1,3 +1,9 @@
+/*
+* Names:     	   Kristina Bradbury	Emilio Hajj	Jim Wu
+* MacIDs: 	   bradbukj	 	hajje		wuzz
+* Student Numbers: 1405897		1402245		Please see submission comment
+* Description: This class calls a public method to retrieve the price of MP3 audios
+*/
 package Shopping;
 public class MP3 extends Audio{  //MP3 inherits all the fields and methods from Audio class
 	@Override
